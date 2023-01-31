20eb1a421b4aace05af66ea3490af486a0a37f42
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-01-31 09:47:04 +0100

    initial commit

---
+++ src/de/demmer/dennis/ApplicationGenerics.java
@@ -0,0 +1,31 @@
+package de.demmer.dennis;
+
+public class ApplicationGenerics {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("----Generic Method:----");
+
+		String[] stringArr = { "Das", "ist", "ein", "Test" };
+		GenericMethodTraining.printArray(stringArr);
+
+		String string1 = GenericMethodTraining.getArgumentBack("Hallo Welt");
+		System.out.println(string1);
+
+		try {
+			String biggestString = GenericMethodTraining.getBiggestVariable("A", "X", "B");
+			System.out.println("Biggest String is: " + biggestString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("----Generic Class:----");
+		GenericClassTraining genericClassWithString = new GenericClassTraining<String>();
+
+		genericClassWithString.setVar("Hallo Welt");
+		System.out.println("Var is: " + genericClass.getVar());
+
+	}
+
+}
