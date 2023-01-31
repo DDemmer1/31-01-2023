3561fe9bd9f40f50dd02258531bbeb450219443c
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-01-31 09:58:32 +0100

    initial commit

--- src/de/demmer/dennis/ApplicationGenerics.java
+++ src/de/demmer/dennis/ApplicationGenerics.java
@@ -7,22 +7,31 @@ public class ApplicationGenerics {
 		
 		System.out.println("----Generic Method:----");
 
-		String[] stringArr = { "Das", "ist", "ein", "Test" };
+		
+		Boolean[] stringArr = { true, false, true};
 		GenericMethodTraining.printArray(stringArr);
 
-		String string1 = GenericMethodTraining.getArgumentBack("Hallo Welt");
-		System.out.println(string1);
+		
+		
+		Double d = GenericMethodTraining.getArgumentBack(5.5);
+		System.out.println(d);
 
+		
+		
 		try {
 			String biggestString = GenericMethodTraining.getBiggestVariable("A", "X", "B");
 			System.out.println("Biggest String is: " + biggestString);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		
 
 		System.out.println("----Generic Class:----");
 		GenericClassTraining<String> genericClassWithString = new GenericClassTraining<String>();
 
+		
+		
 		genericClassWithString.setVar("Hallo Welt");
 		System.out.println("Var is: " + genericClassWithString.getVar());
 
